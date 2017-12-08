d2b0fd22acd394330e2f297d1ba8a0946119b970
Rob Cavanaugh <cavanaugh.rj@gmail.com>
2017-12-08 07:32:10 -0800

    Setting up Grid

---
+++ Edit-Save/Assets/Scripts/Grid.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Grid : MonoBehaviour
+{
+    // Static instance of Grid
+    public static Grid gridInstance = null;
+
+    //Grid properties
+    private GridLayoutGroup grid;
+    public GameObject canvasGameObject;
+    public int gridWidth = 0;
+    public int gridHeight = 0;
+    public GameObject gridPrefab;
+
+
+    //Tiles
+    private Tile[,] tiles;
+    private float tileWidth = -1f;
+    private float tileHeight = -1;
+    public GameObject tileNodePrefab;
+
+    private void Awake()
+    {
+        //Check if instance already exists
+        if (gridInstance != null && gridInstance != this)
+        {
+            //If instance already exists and it's not this:
+            Destroy(gameObject);
+        }
+        else
+        {
+            //Sets this to not be destroyed when reloading scene
+           // DontDestroyOnLoad(gameObject);
+
+            gridInstance = this;
+
+        }
+        PopulateGrid();
+    }
+    void Start ()
+    {
+       
+	}
+	
+    private void PopulateGrid()
+    {
+        if (!grid)
+        {
+            GameObject gridGameObject = Instantiate(gridPrefab);
+            grid = gridGameObject.GetComponent<GridLayoutGroup>();
+            if (canvasGameObject)
+            {
+                gridGameObject.transform.SetParent(canvasGameObject.transform, true);
+            }
+            if (grid)
+            {
+                RectTransform gridRect = grid.GetComponent<RectTransform>();
+                tileWidth = gridRect.rect.width / (float)gridWidth;
+                tileHeight = gridRect.rect.height / (float)gridHeight;
+
+                grid.cellSize = new Vector2(tileWidth, tileHeight);
+
+                tiles = new Tile[gridWidth, gridHeight];
+
+                Debug.Log("GridSetup::Setup Grid: gridWidth: " + gridWidth + " : gridHeight: " + gridHeight + " : tileWidth: " + tileWidth + " : tileHeight: " + tileHeight);
+
+                for (int y = 0; y < gridHeight; y++)
+                {
+
+                    for (int x = 0; x < gridWidth; x++)
+                    {
+                        GameObject buttonObject = Instantiate(tileNodePrefab);
+                        buttonObject.name = "Tile(" + x + ", " + y + ")";
+                        Button button = buttonObject.GetComponent<Button>();
+                        if (button)
+                        {
+                            Tile tile = ScriptableObject.CreateInstance<Tile>();
+                            tile.gameObject = button.gameObject;
+                            button.transform.SetParent(grid.transform, false);
+                            tile.x = x;
+                            tile.y = y;
+
+                            //button.onClick.AddListener(() => TileClicked(tile));
+                            Debug.Log("Created Tile:" + tile.x + "," + tile.y);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Debug.LogError("No Grid !!!");
+            }
+        }
+    }
+
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
---
+++ Edit-Save/Assets/Scripts/Tile.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using Newtonsoft.Json;
+
+public class Tile : ScriptableObject
+{
+    [JsonProperty("X")]
+    public int x { get; set; }
+    [JsonProperty("Y")]
+    public int y { get; set; }
+
+    public Tile() { }
+    public Tile (int _x, int _y)
+    {
+        x = _x;
+        y = _y;
+    }
+    public GameObject gameObject;
+    public enum TileType
+    {
+        FLOOR,
+        OBSTACLE
+    };
+    [JsonProperty("Type")]
+    public TileType Type { get; set; }
+
+}
